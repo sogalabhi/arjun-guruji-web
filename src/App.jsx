9d377e4f87246de56ba383d3c1878db8762f356c
sogalabhi <abhijithsogal@gmail.com>
2024-06-15 22:37:27 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import SidebarNav from "./components/sideBar";
+
+export default function App() {
+   return (
+      <>
+         <SidebarNav />
+
+         <div className="p-4 sm:ml-64 bg-yellow-500 min-h-screen">
+            <h1 className="font-samarkan text-7xl text-center">Arjun Guruji App</h1>
+            <h2 className="text-4xl">Popular Books</h2>
+            <div className="flex">
+               <img src="" alt="" />
+            </div>
+         </div>
+
+      </>
+   )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        samarkan: ["Samarkan", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
+
